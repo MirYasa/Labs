a78d360796f224d99be68259fce0d10aa01f5fc6
Miron Gurevich <gurevich.miron.workmail@gmail.com>
2020-12-11 13:24:46 +0300

    4.1

--- core/EquationInterface.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections.Generic;
-namespace Labs.core {
-    public interface EquationInterface {
-        List<float> Roots (float a, float b, float c);
-    }
-}
--- core/LogInterface.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-namespace Labs.core {
-    public interface LogInterface {
-	static void Log(string str){}
-	static void Write() {}
-    }
-} 
